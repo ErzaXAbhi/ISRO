fda0f4f6b8fa747fc6c458ba34be9e2be1b66308
Abhijit Parad <ErzaXAbhi>
2023-09-13 23:52:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const topRocket = document.querySelector(".top-rocket");
+// const midRocket = document.querySelector(".mid-rocket");
+// const rocket = document.querySelector(".bot-rocket");
+// const supply = document.querySelector(".supply");
+// const blocs = document.querySelectorAll(".bloc");
+
+// const offsets = [105, 206, 203];
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// gsap.utils.toArray([".bloc2", ".bloc4", ".bloc5"]).forEach((bloc, index) => {
+//   gsap.to(bloc, {
+//     y: 0,
+//     ease: "linear",
+//     scrollTrigger: {
+//       trigger: ".container-scroll",
+//       start: "top center", // Adjust this to your needs
+//       end: `+=${offsets[index]}`,
+//       scrub: true,
+//     },
+//   });
+// });
